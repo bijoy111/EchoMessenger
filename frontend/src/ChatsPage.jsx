2550dc5a184f098b19d77ecf46b03c97c194d408
bjjoy111 <bijoysaeem@gmail.com>
2024-08-06 14:53:33 +0600

    modified to the ChatsPage.jsx file

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,10 +1,11 @@
 import { PrettyChatWindow } from "react-chat-engine-pretty";
 
 const ChatsPage = (props) => {
+  const projectId = process.env.REACT_APP_PROJECT_ID;
   return (
     <div className="background">
       <PrettyChatWindow
-        const projectId = {process.env.REACT_APP_PROJECT_ID}
+        const projectId = {projectId}
         username={props.user.username}
         secret={props.user.secret}
       />
